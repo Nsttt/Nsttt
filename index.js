51173729729c64423b4f4545684c48e060e3edbb
Nestor <elnestinatorHD@gmail.com>
2020-09-04 20:22:48 +0200

    Added last refresh and instagram link.

--- index.js
+++ index.js
@@ -1,43 +1,54 @@
-const Mustache = require('mustache');
-const fs = require('fs');
-const puppeteerService = require('./services/puppeteer.service');;
+const Mustache = require("mustache");
+const fs = require("fs");
+const puppeteerService = require("./services/puppeteer.service");
 
-const MUSTACHE_MAIN_DIR = './main.mustache';
+const MUSTACHE_MAIN_DIR = "./main.mustache";
 
 let DATA = {
-
-}
+  refresh_date: new Date().toLocaleDateString("en-GB", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    timeZoneName: "short",
+    timeZone: "Europe/Madrid",
+  }),
+};
 
 async function setInstagramPosts() {
-    const instagramImages = await puppeteerService.getLatestInstagramPostsFromAccount('nstlopez', 3);
-    DATA.img1 = instagramImages[0];
-    DATA.img2 = instagramImages[1];
-    DATA.img3 = instagramImages[2];
-  }
+  const instagramImages = await puppeteerService.getLatestInstagramPostsFromAccount(
+    "nstlopez",
+    3
+  );
+  DATA.img1 = instagramImages[0];
+  DATA.img2 = instagramImages[1];
+  DATA.img3 = instagramImages[2];
+}
 
 function generateReadMe() {
-  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
+  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
     if (err) throw err;
     const output = Mustache.render(data.toString(), DATA);
-    fs.writeFileSync('README.md', output);
+    fs.writeFileSync("README.md", output);
   });
 }
 
 async function action() {
-    /**
-     * Get pictures
-     */
-    await setInstagramPosts();
-  
-    /**
-     * Generate README
-     */
-    await generateReadMe();
-  
-    /**
-     * Fermeture de la boutique 👋
-     */
-    await puppeteerService.close();
-  }
-  
-  action();
+  /**
+   * Get pictures
+   */
+  await setInstagramPosts();
+
+  /**
+   * Generate README
+   */
+  await generateReadMe();
+
+  /**
+   * Fermeture de la boutique 👋
+   */
+  await puppeteerService.close();
+}
+
+action();
